985aeed736cd056365efcc7166cd6971933ed69d
laifu-666 <arina.dmitr06@gmail.com>
2021-03-31 19:44:23 +0300

    31.03.21

---
+++ 08. Дмитриева/31.03.21/scriptt.js
@@ -0,0 +1,23 @@
+//Задание №1
+var temDeg = prompt("Введите температуру по Цельсию:")
+var result = (9/5)*temDeg+32;
+alert (result);
+console.log ("В Фаренгейтах: " + result);
+// 
+
+//Задание №3
+var admin;
+var name="Василий";
+admin = name;
+alert ("Имя: " + admin);
+console.log ("Имя: " + admin);
+// 
+
+//Задание №4
+var digitValue = 1000;
+var textValue="108";
+var additionResult = digitValue + textValue;
+alert ("Результат: " + additionResult);
+console.log ("Результат: " + additionResult);
+//
+
