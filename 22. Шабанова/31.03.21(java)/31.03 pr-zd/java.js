e078ce41a6f59dc3e6b2e9e6d09aa341f67e6466
poolli-777 <margarita.shabanova2211@gmail.com>
2021-03-31 12:01:47 +0300

    30-31.03.21

---
+++ 22. Шабанова/31.03.21(java)/31.03 pr-zd/java.js
@@ -0,0 +1,16 @@
+//Задание №1
+var temDeg = prompt("Enter temperature in degrece:")
+var result = (9/5)*temDeg+32;
+alert (result);
+console.log ("Task 1: temperature in Fahrenheit: " + result);
+// Конец задания №1
+
+//задание 2
+var admin, name;
+var admin;
+var name = 'Василий';
+admin = name;
+alert(admin);
+
+
+
